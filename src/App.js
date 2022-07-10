114e90e9afe6fbf11526ee405bfa22c5248d7a5a
prathmesh <prathmeshjagtap405@gmail.com>
2022-07-10 19:05:35 +0530

    initail  commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<h1 className="text-3xl font-bold underline text-center">
+				Take home Assignemt 1
+			</h1>
+		</div>
+	);
+}
+
+export default App;
